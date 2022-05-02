7bda4701d06908a2e86098c790610c95b9f2e2e2
gigc2 <gigc2@hlhl.rtss.qc.ca>
2022-05-02 07:32:14 -0400

    Used package clipr for clip command because this command would no longer work in R 4.2.0 due to some changes in the encoding of the clipboard

--- R/clip.R
+++ R/clip.R
@@ -1,35 +1,14 @@
-### Send a table-like object to clipboard (Windows only).
+### Send a table-like object to clipboard.
 
-clip  <- function(x, sep = "\t", row.names = FALSE, quote = FALSE, ...){    
-    if (Sys.info()['sysname'] %in% c("Windows")) {
-        write.table(x, file = "clipboard", sep = sep,
-                    row.names = row.names, quote = quote, ...)
-    }
-    else {
-        ## solution for unix taken from
-        ## https://stackoverflow.com/questions/10959521/how-to-write-to-clipboard-on-ubuntu-linux-in-r
-              
-        ## https://stackoverflow.com/a/10960498/3980197
-        write.xclip = function(x) {
-            ## if xclip not installed
-            if (!isTRUE(file.exists(Sys.which("xclip")[1L]))) {
-                stop("Cannot find xclip")
-            }
-            con <- pipe("xclip -selection c", "w")
-            on.exit(close(con))
-            write.table(x, con, sep = sep,
-                        row.names = row.names, quote = quote, ...)
-        }
-
-        tryCatch({
-            write.xclip(x)
-        }, error = function(e) {
-            message("Could not write using xclip")
-        })
-    }
+clip  <- function(x, sep = "\t", row.names = FALSE, quote = FALSE, ...){
+  if(!is.matrix(x) & !is.data.frame(x)){
+    x <- try(as.data.frame(x), silent = TRUE)
+    if(inherits(x, "try-error"))
+      stop("x cannot be coerced to a data.frame object")
+  }
+    
+  
+  clipr::write_clip(x, sep = sep, row.names = row.names, quote = quote,
+                    object_type = "auto", ...)
 }
-
-
-
-
 
